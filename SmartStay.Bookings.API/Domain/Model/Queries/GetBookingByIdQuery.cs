ec7f759790fb37de29580bdc09ded455e28643d8
Samuel Molina <samuelmolina281816@gmail.com>
2026-05-15 22:58:44 -0500

    fix(queries) adding queries comments

--- SmartStay.Bookings.API/Domain/Model/Queries/GetBookingByIdQuery.cs
+++ SmartStay.Bookings.API/Domain/Model/Queries/GetBookingByIdQuery.cs
@@ -1,7 +1,7 @@
 namespace SmartStay.Bookings.API.Domain.Model.Queries;
 
 /// <summary>
-/// Query to retrieve a booking by its identifier.
+/// Query to retrieve a booking by its identifier. 
 /// </summary>
 /// <param name="BookingId">The identifier of the booking to retrieve.</param>
 public record GetBookingByIdQuery(int BookingId);
